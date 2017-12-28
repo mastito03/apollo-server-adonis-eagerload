caa3dcb227fa08eb5953326a7c43a3801b4debe1
mastito <mr.aladin03@gmail.com>
2017-12-28 21:05:46 +0700

    Bump

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports.eagerLoad = function(query, fieldNodes) {
+  function __eagerLoad(query, selectionSet, name) {
+    for (const selection of selectionSet.selections) {
+      if (selection.selectionSet != null) {
+        query = query.with(name + selection.name.value)
+        query = __eagerLoad(
+          query,
+          selection.selectionSet,
+          selection.name.value + '.'
+        )
+      }
+    }
+    return query
+  }
+
+  return __eagerLoad(query, fieldNodes[0].selectionSet, '')
+}
